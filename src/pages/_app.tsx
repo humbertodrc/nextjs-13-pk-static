9876fdfbdc0ea2a460908bb5cc407d87260faed3
humbertodrc <humbertorivero9@gmail.com>
2023-01-15 01:59:09 -0300

    Add nextUI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from "next/app";
+import {NextUIProvider} from "@nextui-org/react";
+import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}: AppProps) {
+	return (
+		<NextUIProvider>
+			<Component {...pageProps} />
+		</NextUIProvider>
+	);
 }
