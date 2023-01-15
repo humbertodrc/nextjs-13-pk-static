66b1c7a3f678eec6acb468102c2ab2b01d9b2084
humbertodrc <humbertorivero9@gmail.com>
2023-01-15 02:25:12 -0300

    Feat dark theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <h1>Hola Mundo</h1>
-      <Button color={'gradient'}>Click me</Button>;
+      <Button color={'gradient'}>Click me</Button>
     </>
   )
 }
